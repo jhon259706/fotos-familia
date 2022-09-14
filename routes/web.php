f6b46e5ebbc5662e12f35cc0233d73dfe6a13315
Jhon Melendez <jmelendez@saatvamattress.com>
2022-09-13 22:42:29 -0500

    Configuring routes

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function() {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('pictures');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
