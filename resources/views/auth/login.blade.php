f75b6bdfb7f1cff14ba03776d02d1de3fa7caa75
Jhon Melendez <jmelendez@saatvamattress.com>
2022-09-14 09:24:10 -0500

    Translating the register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,18 +37,28 @@
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
                 </label>
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 float-right" href="{{ route('password.request') }}">
                         {{ __('Olvidaste tu contraseña?') }}
                     </a>
                 @endif
+            </div>
 
-                <x-button class="ml-3">
+            <div class="flex items-center justify-end mt-4">
+
+
+                <x-button class="ml-3 bg-green-500">
                     {{ __('Ingresar') }}
                 </x-button>
+
+                @if (Route::has('password.request'))
+                    <x-button class="ml-3">
+                        <a href="{{ route('register') }}" >
+                            {{ __('Registrarme') }}
+                        </a>
+                    </x-button>
+                @endif
             </div>
         </form>
     </x-auth-card>
